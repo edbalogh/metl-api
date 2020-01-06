138e24e5130b350f0fb7ee3e46e266c3c6d74313
ebalog <ed.balogh@acxiom.com>
2020-01-06 12:19:00 -0500

    finish up execution settings test for file path

--- test/file/execution-settings-api-file.js
+++ test/file/execution-settings-api-file.js
@@ -61,18 +61,35 @@ describe('ExecutionSettings API File Tests', () => {
     await request(mock).get(endPoint).expect(204);
   });
 
-  it('Should fail validation on insert', async () => {
+  it('Should fail validation on insert with executionTypeId', async () => {
     const response = await request(mock)
       .post(endPoint)
       .send({ name: 'missing executorTypeId', 'executionTypeId': 'local-spark-execution' })
       .expect('Content-Type', /json/)
       .expect(422);
     const apiResponse = JSON.parse(response.text);
+    console.log(JSON.stringify(response.text));
     expect(apiResponse).to.exist;
     expect(apiResponse).to.have.property('errors').lengthOf(1);
     expect(apiResponse).to.have.property('body');
     const errors = apiResponse.errors;
     expect(errors.find(err => err.params.missingProperty === 'sparkSubmitPath')).to.exist;
+    await request(mock).get(endPoint).expect(204);
+  });
+
+  it('Should fail validation on insert without executionTypeId', async () => {
+    const response = await request(mock)
+      .post(endPoint)
+      .send({ name: 'missing executorTypeId' })
+      .expect('Content-Type', /json/)
+      .expect(422);
+    const apiResponse = JSON.parse(response.text);
+    expect(apiResponse).to.exist;
+    console.log(JSON.stringify(response.text));
+    expect(apiResponse).to.have.property('errors').lengthOf(1);
+    expect(apiResponse).to.have.property('body');
+    const errors = apiResponse.errors;
+    expect(errors.find(err => err.params.missingProperty === 'executionTypeId')).to.exist;
     await request(mock).get(endPoint).expect(204);
   });
 
@@ -94,101 +111,101 @@ describe('ExecutionSettings API File Tests', () => {
     returnExecutionSettingsObject = resp['execution-settings'];
     verifyReturnObject(resp['execution-settings'], newSettings);
   });
-  //
-  // it('Should get the inserted execution-settings', async () => {
-  //   const response = await request(mock)
-  //     .get(`/api/v1/execution-settings/${returnExecutionSettingsObject.id}`)
-  //     .expect('Content-Type', /json/)
-  //     .expect(200);
-  //   const resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings');
-  //   verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
-  // });
-  //
-  // it('Should get all execution-settings', async () => {
-  //   const response = await request(mock)
-  //     .get(endPoint)
-  //     .expect('Content-Type', /json/)
-  //     .expect(200);
-  //   const resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings').lengthOf(1);
-  //   verifyReturnObject(resp['execution-settings'][0], returnExecutionSettingsObject)
-  // });
-  //
-  // it('Should update a execution-settings', async () => {
-  //   returnExecutionSettingsObject.executionTypeId = `et1`;
-  //   const response = await request(mock)
-  //     .put(`${endPoint}/${returnExecutionSettingsObject.id}`)
-  //     .send(returnExecutionSettingsObject)
-  //     .expect('Content-Type', /json/)
-  //     .expect(200);
-  //   const resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings');
-  //   expect(resp['execution-settings']).to.have.property('executionTypeId').eq('et1');
-  //   verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
-  // });
-  //
-  // it('Should update a single execution-settings using post', async () => {
-  //     returnExecutionSettingsObject.executionTypeId = 'et2';
-  //     const response = await request(mock)
-  //       .post(endPoint)
-  //       .send(returnExecutionSettingsObject)
-  //       .expect('Content-Type', /json/)
-  //       .expect(201);
-  //     const resp = JSON.parse(response.text);
-  //     expect(resp).to.exist;
-  //     expect(resp).to.have.property('execution-settings');
-  //     expect(resp['execution-settings']).to.have.property('executionTypeId').eq('et2');
-  //   verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
-  //   });
-  //
-  // it('Should upsert a single execution-settings', async () => {
-  //   returnExecutionSettingsObject.executionTypeId = 'et3';
-  //   const response = await request(mock)
-  //     .put(`${endPoint}/${returnExecutionSettingsObject.id}`)
-  //     .send(returnExecutionSettingsObject)
-  //     .expect('Content-Type', /json/)
-  //     .expect(200);
-  //   const resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings');
-  //   expect(resp['execution-settings']).to.have.property('executionTypeId').eq('et3');
-  //   verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
-  // });
-  //
-  // it('Should delete a execution-settings', async () => {
-  //   await request(mock).delete(`${endPoint}/${returnExecutionSettingsObject.id}`).expect(204);
-  //   await request(mock).get(endPoint).expect(204);
-  // });
-  //
-  // it('Should insert multiple execution-settings', async () => {
-  //   const data = [
-  //     { name: 'local1', executionTypeId: 'exec-type-id-1', defaultSparkParameters: [ { parameter: 'p1', value: 'v1' } ] },
-  //     { name: 'local2', executionTypeId: 'exec-type-id-2', defaultConfParameters: [ { parameter: 'p2' } ] },
-  //     { name: 'local3', executionTypeId: 'exec-type-id-2' }
-  //   ];
-  //
-  //   let response = await request(mock)
-  //     .post(endPoint)
-  //     .send(data)
-  //     .expect('Content-Type', /json/)
-  //     .expect(201);
-  //   let resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings').lengthOf(data.length);
-  //   resp['execution-settings'].forEach(x => verifyReturnObject(x, data.find(y => x.name === y.name)));
-  //   response = await request(mock)
-  //     .get(endPoint)
-  //     .expect('Content-Type', /json/)
-  //     .expect(200);
-  //   resp = JSON.parse(response.text);
-  //   expect(resp).to.exist;
-  //   expect(resp).to.have.property('execution-settings').lengthOf(data.length);
-  // });
-  //
+
+  it('Should get the inserted execution-settings', async () => {
+    const response = await request(mock)
+      .get(`${endPoint}/${returnExecutionSettingsObject.id}`)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings');
+    verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
+  });
+
+  it('Should get all execution-settings', async () => {
+    const response = await request(mock)
+      .get(endPoint)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings').lengthOf(1);
+    verifyReturnObject(resp['execution-settings'][0], returnExecutionSettingsObject)
+  });
+
+  it('Should update a execution-settings', async () => {
+    returnExecutionSettingsObject.numberCores = 1;
+    const response = await request(mock)
+      .put(`${endPoint}/${returnExecutionSettingsObject.id}`)
+      .send(returnExecutionSettingsObject)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings');
+    expect(resp['execution-settings']).to.have.property('numberCores').eq(1);
+    verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
+  });
+
+  it('Should update a single execution-settings using post', async () => {
+      returnExecutionSettingsObject.numberCores = 2;
+      const response = await request(mock)
+        .post(endPoint)
+        .send(returnExecutionSettingsObject)
+        .expect('Content-Type', /json/)
+        .expect(201);
+      const resp = JSON.parse(response.text);
+      expect(resp).to.exist;
+      expect(resp).to.have.property('execution-settings');
+      expect(resp['execution-settings']).to.have.property('numberCores').eq(2);
+    verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
+    });
+
+  it('Should upsert a single execution-settings', async () => {
+    returnExecutionSettingsObject.numberCores = 3;
+    const response = await request(mock)
+      .put(`${endPoint}/${returnExecutionSettingsObject.id}`)
+      .send(returnExecutionSettingsObject)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings');
+    expect(resp['execution-settings']).to.have.property('numberCores').eq(3);
+    verifyReturnObject(resp['execution-settings'], returnExecutionSettingsObject);
+  });
+
+  it('Should delete a execution-settings', async () => {
+    await request(mock).delete(`${endPoint}/${returnExecutionSettingsObject.id}`).expect(204);
+    await request(mock).get(endPoint).expect(204);
+  });
+
+  it('Should insert multiple execution-settings', async () => {
+    const data = [
+      { name: 'local1', executionTypeId: 'local-spark-execution', sparkSubmitPath: '/usr/local/bin/', numberCores: 4, deployMode: 'cluster' },
+      { name: 'local2', executionTypeId: 'local-spark-execution', sparkSubmitPath: '/usr/bin/', numberCores: 3, deployMode: 'cluster' },
+      { name: 'local3', executionTypeId: 'local-spark-execution', sparkSubmitPath: '/bin/', numberCores: 2, deployMode: 'cluster' }
+    ];
+
+    let response = await request(mock)
+      .post(endPoint)
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(201);
+    let resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings').lengthOf(data.length);
+    resp['execution-settings'].forEach(x => verifyReturnObject(x, data.find(y => x.name === y.name)));
+    response = await request(mock)
+      .get(endPoint)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    resp = JSON.parse(response.text);
+    expect(resp).to.exist;
+    expect(resp).to.have.property('execution-settings').lengthOf(data.length);
+  });
+
   function verifyReturnObject(inObj, original) {
     expect(inObj).to.have.property('id');
     if(original.hasOwnProperty('id')) {
